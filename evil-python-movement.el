9419ad376040d94f732d021847a7098c0e637e0b
Felipe Lema <felipelema@mortemale.org>
2018-06-19 10:01:01 -0400

    Comply with packaging

--- evil-python-movement.el
+++ evil-python-movement.el
@@ -5,6 +5,8 @@
 ;; Author: Felipe Lema <felipelema en mortemale punto org>
 ;; Keywords:
 
+;; Version: 1.0
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
